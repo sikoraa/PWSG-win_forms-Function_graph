828c15d06654bd0eecc065c5f9798b5be8c49af6
sikoraa <sikoraa@protonmail.com>
2018-06-05 21:48:28 +0200

    Add project files.

---
+++ surprise/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace surprise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.mapa = new System.Windows.Forms.PictureBox();
+            this.funcBox = new System.Windows.Forms.CheckedListBox();
+            this.drawB = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fromBox = new System.Windows.Forms.TextBox();
+            this.toBox = new System.Windows.Forms.TextBox();
+            this.domainB = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.minY = new System.Windows.Forms.Label();
+            this.maxY = new System.Windows.Forms.Label();
+            this.asyncB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mapa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.progressBar1.Location = new System.Drawing.Point(0, 510);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1174, 39);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // mapa
+            // 
+            this.mapa.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.mapa.Location = new System.Drawing.Point(0, 0);
+            this.mapa.Name = "mapa";
+            this.mapa.Size = new System.Drawing.Size(1024, 512);
+            this.mapa.TabIndex = 1;
+            this.mapa.TabStop = false;
+            // 
+            // funcBox
+            // 
+            this.funcBox.FormattingEnabled = true;
+            this.funcBox.Location = new System.Drawing.Point(1031, 13);
+            this.funcBox.Name = "funcBox";
+            this.funcBox.Size = new System.Drawing.Size(130, 94);
+            this.funcBox.TabIndex = 2;
+            this.funcBox.SelectedIndexChanged += new System.EventHandler(this.funcBox_SelectedIndexChanged);
+            // 
+            // drawB
+            // 
+            this.drawB.Location = new System.Drawing.Point(1031, 130);
+            this.drawB.Name = "drawB";
+            this.drawB.Size = new System.Drawing.Size(130, 23);
+            this.drawB.TabIndex = 3;
+            this.drawB.Text = "Draw";
+            this.drawB.UseVisualStyleBackColor = true;
+            this.drawB.Click += new System.EventHandler(this.drawB_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1028, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1030, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To";
+            // 
+            // fromBox
+            // 
+            this.fromBox.Location = new System.Drawing.Point(1061, 160);
+            this.fromBox.Name = "fromBox";
+            this.fromBox.Size = new System.Drawing.Size(100, 20);
+            this.fromBox.TabIndex = 6;
+            this.fromBox.Text = "-2";
+            // 
+            // toBox
+            // 
+            this.toBox.Location = new System.Drawing.Point(1061, 193);
+            this.toBox.Name = "toBox";
+            this.toBox.Size = new System.Drawing.Size(100, 20);
+            this.toBox.TabIndex = 7;
+            this.toBox.Text = "3";
+            // 
+            // domainB
+            // 
+            this.domainB.Location = new System.Drawing.Point(1033, 232);
+            this.domainB.Name = "domainB";
+            this.domainB.Size = new System.Drawing.Size(128, 23);
+            this.domainB.TabIndex = 8;
+            this.domainB.Text = "Change domain";
+            this.domainB.UseVisualStyleBackColor = true;
+            this.domainB.Click += new System.EventHandler(this.domainB_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1031, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Max Y: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1033, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Min Y: ";
+            // 
+            // minY
+            // 
+            this.minY.AutoSize = true;
+            this.minY.Location = new System.Drawing.Point(1079, 310);
+            this.minY.Name = "minY";
+            this.minY.Size = new System.Drawing.Size(0, 13);
+            this.minY.TabIndex = 11;
+            // 
+            // maxY
+            // 
+            this.maxY.AutoSize = true;
+            this.maxY.Location = new System.Drawing.Point(1080, 279);
+            this.maxY.Name = "maxY";
+            this.maxY.Size = new System.Drawing.Size(0, 13);
+            this.maxY.TabIndex = 12;
+            // 
+            // asyncB
+            // 
+            this.asyncB.Location = new System.Drawing.Point(1036, 344);
+            this.asyncB.Name = "asyncB";
+            this.asyncB.Size = new System.Drawing.Size(125, 23);
+            this.asyncB.TabIndex = 13;
+            this.asyncB.Text = "Draw async";
+            this.asyncB.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 547);
+            this.Controls.Add(this.asyncB);
+            this.Controls.Add(this.maxY);
+            this.Controls.Add(this.minY);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.domainB);
+            this.Controls.Add(this.toBox);
+            this.Controls.Add(this.fromBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawB);
+            this.Controls.Add(this.funcBox);
+            this.Controls.Add(this.mapa);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Integral1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.mapa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox mapa;
+        private System.Windows.Forms.CheckedListBox funcBox;
+        private System.Windows.Forms.Button drawB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fromBox;
+        private System.Windows.Forms.TextBox toBox;
+        private System.Windows.Forms.Button domainB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label minY;
+        private System.Windows.Forms.Label maxY;
+        private System.Windows.Forms.Button asyncB;
+    }
+}
+
---
+++ surprise/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace surprise
+{
+    public partial class Form1 : Form
+    {
+        string[] functions;
+        int nr = 10000; // ilosc pktow 
+        double MINY = double.MaxValue;
+        double MAXY = double.MinValue;
+        double[] x;
+        double[,] y;
+        Point[,] p;
+        int n = 3;
+        bool[] selF;
+        double x1, x2;
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+           
+            x1 = double.Parse(fromBox.Text);
+            x2 = double.Parse(toBox.Text);
+            string[] tmpfunctions = { "x*x*x-x*x-x-1", "x*x", "x - 6" };
+            Color[] colors = { Color.Red, Color.Blue, Color.Black, Color.Orange, Color.Green, Color.Purple  };
+            functions = tmpfunctions;
+            n = functions.Length;
+            x = new double[nr];
+            y = new double[n, x.Length];
+            selF = new bool[n];
+            selF[0] = true;
+            p = new Point[n, x.Length];
+            funcBox.Items.AddRange(functions);      
+            funcBox.SetItemChecked(0, true);
+            this.Refresh();
+        }
+        
+        private void funcBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < functions.Length; ++i)
+            {
+                selF[i] = false;
+                if (funcBox.GetItemChecked(i))
+                {
+                    selF[i] = true;
+                }
+            }
+        }
+
+        private void domainB_Click(object sender, EventArgs e)
+        {
+            double from = 0;
+            double to = 0;
+            if (!double.TryParse(fromBox.Text, out from) || !double.TryParse(toBox.Text, out to))
+            {
+                fromBox.Text = x1.ToString();
+                toBox.Text = x2.ToString();
+                MessageBox.Show("Please check if domain is a numeric value");
+                return;
+            }
+            if (from >= to)
+            {
+                fromBox.Text = x1.ToString();
+                toBox.Text = x2.ToString();
+                MessageBox.Show("Please check that From is lesser than To");
+                return;
+            }
+            fromBox.Text.Trim();
+            toBox.Text.Trim();
+            
+            x1 = from; x2 = to;
+            double dx = (x2 - x1) / (nr - 1);
+            if (nr > 0) x[0] = x1;
+            for (int i = 1; i < nr; ++i)
+            {
+                x[i] = x[i-1] + dx;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.Size = new Size(1189, 585);
+            //this.Refresh();
+        }
+
+        private Point conv(double x, double y)
+        {
+            int originalStartx = (int)x1; int originalEndx = (int)x2; // original range
+    int newStartx = 0;
+            int newEndx = 1023; // desired range // value to convert
+
+            double scalex = (double)(newEndx - newEndx) / (originalEndx-originalStartx);
+            int originalStarty = (int)MINY; int originalEndy = (int)MAXY; // original range
+            int newStarty = 0;
+            int newEndy = 511; // desired range // value to convert
+
+            double scaley = (double)(newEndy - newEndy) / (originalEndy - originalStarty);
+            //double tx = 
+            Point PP = new Point((int)(newStartx + (x - originalStartx) * scalex), (int)y);
+            return PP;
+        }
+
+        private void drawB_Click(object sender, EventArgs e)
+        {
+            bool any = false;
+            Pen pTmp = new Pen(Color.Red, (float)2);
+            Bitmap bmp = new Bitmap(mapa.Width, mapa.Height);
+            Graphics gr = mapa.CreateGraphics();          
+            Graphics rg = Graphics.FromImage(bmp);
+
+            rg.SmoothingMode = SmoothingMode.AntiAlias;
+                        rg.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+                        rg.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+
+
+
+            for (int i = 0; i < functions.Length; ++i)
+            {
+                if (selF[i]) // funkcja zaznaczona do narysowania
+                {
+                    any = true;
+                    for (int j = 0; j < nr; ++j)
+                    {
+                        //y[i, j] = // 0;
+                        switch (i)
+                        {
+                            case 0:
+                                y[i, j] = x[j] * x[j] * x[j] - x[j] * x[j] - x[j] - 1;
+                                break;
+                            case 1:
+                                y[i, j] = x[j]*x[j];
+                                break;
+                            case 2:
+                                y[i, j] = x[j]-6;
+                                break;
+                        }
+                        if (y[i, j] > MAXY) MAXY = y[i, j];
+                        if (y[i, j] < MINY) MINY = y[i, j];
+                        //new Point((int)x[j], (int)y[i, j]);    
+                    }
+                }
+            }
+            for(int i = 0; i < functions.Length; ++i)
+            {
+                if (selF[i])
+                {
+                    for (int j = 0; j < nr; ++j)
+                    {
+                        p[i, j] = conv(x[j], y[i, j]);
+                        rg.DrawEllipse(pTmp, p[i, j].X, p[i, j].Y, (float)1, (float)1);
+                        if (j > 0)
+                            rg.DrawLine(pTmp, p[i, j - 1], p[i, j]);
+                    }
+                }
+            }
+            if (!any)
+            {
+                MAXY = double.MinValue;
+                MINY = double.MaxValue;
+                maxY.Text = "";
+                minY.Text = "";
+            }
+
+            if (mapa.Image != null)
+                mapa.Image.Dispose();
+            mapa.Image = (Image)bmp.Clone();
+            pTmp.Dispose();
+            rg.Dispose();
+            bmp.Dispose();
+            gr.Dispose();
+                         //tmp.Dispose();
+
+        }
+    }
+}
